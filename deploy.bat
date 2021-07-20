384950044403cd9751f2b470066eea26b353d104
Asthor Bjornsson <asthor02@gmail.com>
2021-07-20 18:45:55 +0000

    change deploy script

--- deploy.bat
+++ deploy.bat
@@ -2,10 +2,9 @@
 title deploy
 cd dist
 git init
-git branch gh-pages
 git add .
 git commit -m 'deploy'
 git remote add origin https://github.com/label-iceland/arent.git
-git push -f --set-upstream origin gh-pages
+git push -f -u --set-upstream origin gh-pages
 cd ..
 echo finished deploying!
