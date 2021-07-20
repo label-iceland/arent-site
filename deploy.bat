8c90aa0254816db5108f1715a1b1ba0eb3eaf806
Asthor Bjornsson <asthor02@gmail.com>
2021-07-20 18:18:58 +0000

    'deploy'

---
+++ deploy.bat
@@ -0,0 +1,11 @@
+@echo off
+title deploy
+cd dist
+git init
+git branch gh-pages
+git add .
+git commit -m 'deploy'
+git remote add origin https://github.com/label-iceland/arent.git
+git push -f --set-upstream origin master
+cd ..
+echo finished deploying!
